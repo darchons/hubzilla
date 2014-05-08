15a4c86e955d8884bd394f7c32489890ea3d0003
Jim Chen <nchen@mozilla.com>
2014-05-07 23:03:46 -0400

    Change to using the Bugzilla MyDashboard extension API

--- bootstrap.js
+++ bootstrap.js
@@ -16,10 +16,12 @@ XPCOMUtils.defineLazyGetter(this, "Strings", function() {
   return Services.strings.createBundle("chrome://hubzilla/locale/hubzilla.properties");
 });
 
-const BUGZILLA_API_URL = "https://bugzilla.mozilla.org/rest/";
+const BUGZILLA_API_URL = "https://bugzilla.mozilla.org/jsonrpc.cgi";
 const BUGZILLA_BUG_URL = "https://bugzil.la/";
-const PREFS = "extensions.hubzilla.";
-const PREFS_LOGIN = PREFS + "login";
+const IMG_ALERT_URL = "https://bugzilla.mozilla.org/" +
+                      "skins/contrib/Mozilla/bugzilla-questionmark2.png";
+const IMG_ERROR_URL = "https://bugzilla.mozilla.org/" +
+                      "skins/contrib/Mozilla/bugzilla-person-alternate.png";
 
 function optionsCallback() {
   return {
@@ -31,27 +33,34 @@ function optionsCallback() {
   };
 }
 
+let seqno = 0;
+
 // An XHR request to fetch data for panel.
-function fetchData(url) {
+function fetchData(request) {
   let deferred = Promise.defer();
   let xhr = Cc["@mozilla.org/xmlextras/xmlhttprequest;1"].createInstance(Ci.nsIXMLHttpRequest);
   try {
     xhr.mozBackgroundRequest = true;
-    xhr.open("GET", url, true);
+    xhr.open("POST", BUGZILLA_API_URL, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
     xhr.setRequestHeader("Accept", "application/json");
     xhr.overrideMimeType("application/json");
     xhr.responseType = "json";
-    xhr.onerror = function onerror(e) {
+    xhr.onerror = function(e) {
       deferred.reject(e);
     };
-    xhr.onload = function onload(event) {
-      if (xhr.status === 200) {
-        deferred.resolve(xhr.response);
+    xhr.onload = function() {
+      if (xhr.response && xhr.response.result) {
+        deferred.resolve(xhr.response.result);
+      } else if (xhr.response && xhr.response.error) {
+        deferred.reject(xhr.response.error);
       } else {
-        deferred.reject(xhr.status);
+        deferred.reject("Request status " + xhr.status);
       }
     };
-    xhr.send(null);
+    request.version = "1.0";
+    request.id = ++seqno;
+    xhr.send(JSON.stringify(request));
   } catch (e) {
     deferred.reject(e);
   }
@@ -59,27 +68,49 @@ function fetchData(url) {
 }
 
 function refreshDataset() {
-  let login = Services.prefs.getCharPref(PREFS_LOGIN);
-  if (!login) {
-    return;
-  }
+  let storage = HomeProvider.getStorage(DATASET_ID);
   Task.spawn(function() {
-    let storage = HomeProvider.getStorage(DATASET_ID);
     yield storage.deleteAll();
 
-    let assigned = yield fetchData(BUGZILLA_API_URL +
-        "bug?assigned_to=" + encodeURIComponent(login) +
-        "&limit=50&include_fields=id,summary");
-    if (!assigned) {
-      return;
-    }
+    let requestee = yield fetchData({
+        method: "MyDashboard.run_flag_query",
+        params: { type: "requestee" }
+    });
 
-    yield storage.save(assigned.bugs.map(bug => ({
-      url: BUGZILLA_BUG_URL + bug.id,
-      title: Strings.formatStringFromName("assigned-title", [bug.id + ""], 1),
-      description: bug.summary,
+    yield storage.save(requestee.result.requestee.map(request => ({
+      url: BUGZILLA_BUG_URL + request.bug_id,
+      title: Strings.formatStringFromName("requestee-title",
+        [request.type + request.status, request.requester], 2),
+      description: Strings.formatStringFromName("requestee-desc",
+        [request.bug_id + "", request.bug_summary], 2),
+      image_url: IMG_ALERT_URL,
     })));
-  }).then(null, e => Cu.reportError("Error refreshing dataset " + DATASET_ID + ": " + e));
+
+    let assigned = yield fetchData({
+        method: "MyDashboard.run_bug_query",
+        params: { query: "assignedbugs" }
+    });
+
+    yield storage.save(assigned.result.bugs.map(bug => ({
+      url: BUGZILLA_BUG_URL + bug.bug_id,
+      title: Strings.formatStringFromName("bug-title",
+        [bug.bug_id + "", bug.bug_status], 2),
+      description: bug.short_desc,
+    })));
+
+  }).then(null, e => {
+    Cu.reportError("Error refreshing dataset " + DATASET_ID + ": " + e);
+
+    let title = e.name || ("" + e);
+    let desc = e.message || "";
+
+    return storage.save([{
+      url: BUGZILLA_BUG_URL,
+      title: title,
+      description: desc,
+      image_url: IMG_ERROR_URL,
+    }]);
+  });
 }
 
 function deleteDataset() {
@@ -88,14 +119,6 @@ function deleteDataset() {
     yield storage.deleteAll();
   }).then(null, e => Cu.reportError("Error deleting data from HomeProvider: " + e));
 }
-
-const PrefsObserver = {
-  observe: function(aSubject, aTopic, aData) {
-    if (aTopic === "addon-options-hidden" && aData === PANEL_ID) {
-      HomeProvider.requestSync(DATASET_ID, refreshDataset);
-    }
-  }
-};
 
 /**
  * bootstrap.js API
@@ -120,8 +143,6 @@ function startup(data, reason) {
 
   // Update data once every hour.
   HomeProvider.addPeriodicSync(DATASET_ID, 3600, refreshDataset);
-
-  Services.obs.addObserver(PrefsObserver, "addon-options-hidden", false);
 }
 
 function shutdown(data, reason) {
@@ -134,8 +155,6 @@ function shutdown(data, reason) {
   Home.panels.unregister(PANEL_ID);
 
   HomeProvider.removePeriodicSync(DATASET_ID);
-
-  Services.obs.removeObserver(PrefsObserver, "addon-options-hidden");
 }
 
 function install(data, reason) {}
